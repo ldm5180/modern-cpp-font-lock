2b3c3825f4cff2ccc30f8a2afe995dc69e55d98a
Ludwig PACIFICI <ludwig@lud.cc>
2018-01-07 21:08:34 +0000

    Typo

--- modern-cpp-font-lock.el
+++ modern-cpp-font-lock.el
@@ -12,7 +12,7 @@
 
 ;;; Commentary:
 
-;; Syntax highlighting support for "Modern C++" - until C++17 and
+;; Syntax highlighting support for "Modern C++" - until C++20 and
 ;; Technical Specification. This package aims to provide a simple
 ;; highlight of the C++ language without dependency.
 
